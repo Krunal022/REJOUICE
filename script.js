9a2b4d94e50cf95c0d2c6fd89e84acc9512c88d4
Krunal022 <krunalvaghela723@gmail.com>
2024-06-04 13:34:25 +0530

    Integrated GSAP loader animation.

--- script.js
+++ script.js
@@ -1,135 +1,166 @@
-function locoScroll(){
-    
-gsap.registerPlugin(ScrollTrigger);
+function loaderAnimation() {
+    var tl = gsap.timeline()
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+    tl.from("#loader h3", {
+        x: 40,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.1
+    })
+    tl.to("#loader h3", {
+        opacity: 0,
+        x: -20,
+        duration: 1,
+        stagger: 0.1
+    })
+    tl.to("#loader", {
+        opacity: 0
+    })
+    tl.from("#page1-content h1 span", {
+        y: 100,
+        opacity: 0,
+        stagger: 0.1,
+        duration: 0.5,
+        delay: -0.5
+    })
+    tl.to("#loader", {
+        display: "none"
+    })
+}
+loaderAnimation()
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+function locoScroll() {
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-});
+    gsap.registerPlugin(ScrollTrigger);
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
 
 }
 locoScroll()
 
-function cursorEffect(){
+function cursorEffect() {
     var page1Content = document.querySelector("#page1-content")
     var cursor = document.querySelector("#cursor")
 
-page1Content.addEventListener("mousemove",function(dets){
-    gsap.to(cursor,{
-        x:dets.x,
-        y:dets.y
+    page1Content.addEventListener("mousemove", function (dets) {
+        gsap.to(cursor, {
+            x: dets.x,
+            y: dets.y
+        })
     })
-})
 
-page1Content.addEventListener("mouseenter",function(){
-    gsap.to(cursor,{
-        scale:1,
-        opacity:1
+    page1Content.addEventListener("mouseenter", function () {
+        gsap.to(cursor, {
+            scale: 1,
+            opacity: 1
+        })
     })
-})
 
-page1Content.addEventListener("mouseleave",function(){
-    gsap.to(cursor,{
-        scale:0,
-        opacity:0
+    page1Content.addEventListener("mouseleave", function () {
+        gsap.to(cursor, {
+            scale: 0,
+            opacity: 0
+        })
     })
-})
 
 }
 cursorEffect()
 
-function page2textEffect(){
+function page2textEffect() {
     const myText = new SplitType('.page2-h1 , h2')
 
-    gsap.to('.word',{
-        y:0,
-        stagger:0.05,
-        delay:0.2,
-        duration:.1,
-        scrollTrigger:{
-            trigger:"#page2",
-            scroller:"#main",
-            start:"top 83%",
-            end:"top 85%",
+    gsap.to('.word', {
+        y: 0,
+        stagger: 0.05,
+        delay: 0.2,
+        duration: .1,
+        scrollTrigger: {
+            trigger: "#page2",
+            scroller: "#main",
+            start: "top 83%",
+            end: "top 85%",
             // markers:true,
-            scrub:2
+            scrub: 2
         }
     })
 }
 page2textEffect()
 
-function page4textEffect(){
+function page4textEffect() {
     const myText = new SplitType('.page4-h1 , .page4-h2')
 
-    gsap.to('.word',{
-        y:0,
-        stagger:0.05,
-        delay:0.2,
-        duration:.1,
-        scrollTrigger:{
-            trigger:"#page4",
-            scroller:"#main",
-            start:"top 83%",
-            end:"top 85%",
+    gsap.to('.word', {
+        y: 0,
+        stagger: 0.05,
+        delay: 0.2,
+        duration: .1,
+        scrollTrigger: {
+            trigger: "#page4",
+            scroller: "#main",
+            start: "top 83%",
+            end: "top 85%",
             // markers:true,
-            scrub:2
+            scrub: 2
         }
     })
 }
 page4textEffect()
 
-function page4VideoCursor(){
+function page4VideoCursor() {
 
     var pageVideo = document.querySelector("#loopVideo")
     var cursorx = document.querySelector("#page4-cursor")
 
-        
-    pageVideo.addEventListener("mousemove",function(dets){
-        gsap.to(cursorx,{
-            y:dets.y,
-            x:dets.x,
+
+    pageVideo.addEventListener("mousemove", function (dets) {
+        gsap.to(cursorx, {
+            y: dets.y,
+            x: dets.x,
         })
     })
 
-    pageVideo.addEventListener("mouseenter",function(){
-        gsap.to(cursorx,{
-            scale:1,
-            opacity:1
+    pageVideo.addEventListener("mouseenter", function () {
+        gsap.to(cursorx, {
+            scale: 1,
+            opacity: 1
         })
     })
 
-    pageVideo.addEventListener("mouseleave",function(){
-        gsap.to(cursorx,{
-            scale:0,
-            opacity:0
+    pageVideo.addEventListener("mouseleave", function () {
+        gsap.to(cursorx, {
+            scale: 0,
+            opacity: 0
         })
     })
 }
 page4VideoCursor()
 
-function sliderAnimation(){
+function sliderAnimation() {
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -137,7 +168,9 @@ function sliderAnimation(){
         autoplay: {
             delay: 2500,
             disableOnInteraction: true,
-          },
-      });
+        },
+    });
 }
 sliderAnimation()
+
+
